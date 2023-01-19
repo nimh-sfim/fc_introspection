f05afc93ed53ebdce7494531e39367a296dee62e
javiergcas <javiergcas@gmail.com>
2023-01-19 17:20:13 -0500

    new variables for motion censoring

--- notebooks/utils/basics.py
+++ notebooks/utils/basics.py
@@ -2,7 +2,8 @@ import os.path as osp
 
 # Top of directory structure
 BIOWULF_SHARE_FOLDER = '/data/SFIMJGC_Introspec/'
-PRJ_DIR         = osp.join(BIOWULF_SHARE_FOLDER,'2023_fc_introspection')
+PRJ_DIR              = osp.join(BIOWULF_SHARE_FOLDER,'2023_fc_introspection')
+DATA_DIR             = osp.join(BIOWULF_SHARE_FOLDER,'pdn')
 
 # Folders with original data (fmri and behav)
 ORIG_FMRI_DIR        = '/data/DSST/MPI_LEMON/ds000221-download/'
@@ -15,3 +16,7 @@ PREPROCESSING_NOTES_DIR = osp.join(PRJ_DIR,'code','fc_introspection','resources'
 
 # Resources folders
 RESOURCES_DINFO_DIR = osp.join(PRJ_DIR,'resources/dataset_info')
+
+# QA Configuration
+FINAL_NUM_VOLS    = 652 # Number of volumes in fully pre-processed runs
+REL_MOT_THRESHOLD = 0.2 # Maximum Relative Displacement (in mm)
