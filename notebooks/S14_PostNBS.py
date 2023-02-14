88e1a59f4fb144bbf035842180d3dd12fa14f7bd
javiergcas <javiergcas@gmail.com>
2023-02-14 10:17:26 -0500

    S14 now also shows results as circos plots

--- notebooks/S14_PostNBS.py
+++ notebooks/S14_PostNBS.py
@@ -18,13 +18,13 @@ import numpy as np
 import os.path as osp
 import hvplot.pandas
 from utils.basics import FB_ATLAS_NAME, ATLASES_DIR, CORTICAL_ATLAS_NAME
-from utils.plotting import hvplot_fc, plot_fc, hvplot_fc_nwlevel
+from utils.plotting import hvplot_fc, plot_fc, hvplot_fc_nwlevel, plot_as_circos
 import holoviews as hv
 from holoviews import opts
 
 # # 2. Load information about the Atlas and ROI needed for plotting
 
-SOLUTION = 'CL02_0p005'
+SOLUTION = 'CL02_0p001'
 ATLAS_NAME = FB_ATLAS_NAME
 
 ATLASINFO_PATH = osp.join(ATLASES_DIR,ATLAS_NAME,'{ATLAS_NAME}.roi_info.csv'.format(ATLAS_NAME=ATLAS_NAME))
@@ -32,13 +32,13 @@ roi_info       = pd.read_csv(ATLASINFO_PATH)
 
 data_f1GTf2 = np.loadtxt('/data/SFIMJGC_Introspec/2023_fc_introspection/code/fc_introspection/resources/nbs/{ATLAS_NAME}/NBS_{s}_Results/NBS_{s}_F1gtF2.edge'.format(s=SOLUTION, ATLAS_NAME=ATLAS_NAME))
 data_f1GTf2 = pd.DataFrame(data_f1GTf2,
-                           index   = roi_info.set_index(['Hemisphere','Network','ROI_Name']).index, 
-                           columns = roi_info.set_index(['Hemisphere','Network','ROI_Name']).index)
+                           index   = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID']).index, 
+                           columns = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID']).index)
 
 data_f2GTf1 = np.loadtxt('/data/SFIMJGC_Introspec/2023_fc_introspection/code/fc_introspection/resources/nbs/{ATLAS_NAME}/NBS_{s}_Results/NBS_{s}_F2gtF1.edge'.format(s=SOLUTION, ATLAS_NAME=ATLAS_NAME))
 data_f2GTf1 = pd.DataFrame(data_f2GTf1,
-                           index   = roi_info.set_index(['Hemisphere','Network','ROI_Name']).index, 
-                           columns = roi_info.set_index(['Hemisphere','Network','ROI_Name']).index)
+                           index   = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID']).index, 
+                           columns = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID']).index)
 
 data_f1GTf2
 
@@ -51,3 +51,9 @@ hvplot_fc_nwlevel(data_f1GTf2+data_f2GTf1, title='Between-Group Differences',add
 
 hvplot_fc_nwlevel(data_f1GTf2, mode='count', title='F1 > F2') + hvplot_fc_nwlevel(data_f2GTf1, mode='count', title='F2 > F1') + \
 hvplot_fc_nwlevel(data_f1GTf2+data_f2GTf1,  mode='count', title='')
+
+# ***
+
+data = data_f1GTf2 - data_f2GTf1
+
+plot_as_circos(data,roi_info, edge_weight=.5, figsize=(7,7))
