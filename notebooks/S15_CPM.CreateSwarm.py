4d61e19e74ad635d9ace0743b4f9d73c7a48b89b
javiergcas <javiergcas@gmail.com>
2023-03-07 09:26:43 -0500

    first ridge version

--- notebooks/S15_CPM.CreateSwarm.py
+++ notebooks/S15_CPM.CreateSwarm.py
@@ -24,18 +24,18 @@ import os
 from datetime import datetime
 import getpass
 from utils.basics import get_sbj_scan_list
-from utils.basics import PRJ_DIR, SCRIPTS_DIR, RESOURCES_CPM_DIR, DATA_DIR, FB_400ROI_ATLAS_NAME
+from utils.basics import PRJ_DIR, SCRIPTS_DIR, RESOURCES_CPM_DIR, DATA_DIR, FB_400ROI_ATLAS_NAME, FB_200ROI_ATLAS_NAME
 from cpm.cpm import read_fc_matrices
 
 CPM_NITERATIONS      = 100       # Number of iterations on real data (to evaluate robustness against fold generation)
 CPM_NULL_NITERATIONS = 10000     # Number of iterations used to build a null distribution
 CORR_TYPE            = 'pearson'      # Correlation type to use on the edge-selection step
-E_SUMMARY_METRIC     = 'sum'           # How to summarize across selected edges on the final model
+E_SUMMARY_METRIC     = 'ridge'           # How to summarize across selected edges on the final model
 E_THR_P              = 0.01            # Threshold used on the edge-selection step
 SPLIT_MODE           = 'subject_aware' # Split mode for cross validation
-MODEL_TYPE           = 'pearson_sum'
+MODEL_TYPE           = CORR_TYPE+'_'+E_SUMMARY_METRIC
 CONFOUNDS            = 'conf_not_residualized'
-ATLAS                = FB_400ROI_ATLAS_NAME
+ATLAS_NAME           = FB_200ROI_ATLAS_NAME
 
 username = getpass.getuser()
 print('++ INFO: user working now --> %s' % username)
@@ -59,11 +59,11 @@ print(targets)
 
 # 3. Load FC data into memory
 
-fc_data = read_fc_matrices(scan_list,DATA_DIR,FB_400ROI_ATLAS_NAME)
+fc_data = read_fc_matrices(scan_list,DATA_DIR,ATLAS_NAME)
 
 # 4. Save FC data in vectorized form for all scans into a single file for easy access for batch jobs
 
-out_path = osp.join(RESOURCES_CPM_DIR,'fc_data_{atlas}.csv'.format(atlas=ATLAS))
+out_path = osp.join(RESOURCES_CPM_DIR,f'fc_data_{ATLAS_NAME}.csv')
 fc_data.to_csv(out_path)
 print('++ INFO: FC data saved to disk [%s]' % out_path)
 
@@ -94,8 +94,8 @@ if not osp.exists(logs_folder):
     print('++ INFO: New folder for log files created [%s]' % swarm_folder)
 
 for TARGET in targets:    
-    swarm_path[TARGET]  = osp.join(swarm_folder,'S15_CPM-{atlas}-real-{sm}-{conf}-{mt}-{target}.SWARM.sh'.format(atlas=ATLAS,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
-    logdir_path[TARGET] = osp.join(logs_folder, 'S15_CPM-{atlas}-real-{sm}-{conf}-{mt}-{target}.logs'.format(atlas=ATLAS,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
+    swarm_path[TARGET]  = osp.join(swarm_folder,'S15_CPM-{atlas}-real-{sm}-{conf}-{mt}-{target}.SWARM.sh'.format(atlas=ATLAS_NAME,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
+    logdir_path[TARGET] = osp.join(logs_folder, 'S15_CPM-{atlas}-real-{sm}-{conf}-{mt}-{target}.logs'.format(atlas=ATLAS_NAME,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
 # -
 
 # create specific folders if needed
@@ -112,15 +112,15 @@ for TARGET in targets:
     swarm_file.write('#Create Time: %s' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     swarm_file.write('\n')
     # Insert comment line with SWARM command
-    swarm_file.write('#swarm -f {swarm_path} -g 8 -t 8 -b 10 --time 00:15:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path[TARGET],logdir_path=logdir_path[TARGET]))
+    swarm_file.write('#swarm -f {swarm_path} -g 8 -t 8 -b 10 --time 00:24:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path[TARGET],logdir_path=logdir_path[TARGET]))
     swarm_file.write('\n')
     for n_iter in range(CPM_NITERATIONS):
-        out_dir = osp.join(RESOURCES_CPM_DIR,'swarm_outputs','real',ATLAS,SPLIT_MODE,CONFOUNDS,MODEL_TYPE,TARGET)
+        out_dir = osp.join(RESOURCES_CPM_DIR,'swarm_outputs','real',ATLAS_NAME,SPLIT_MODE,CONFOUNDS,MODEL_TYPE,TARGET)
         if not osp.exists(out_dir):
             os.makedirs(out_dir)
         swarm_file.write("export BEHAV_PATH={behav_path} FC_PATH={fc_path} OUT_DIR={output_dir} BEHAVIOR={behavior} NUM_FOLDS={k} NUM_ITER={n_iter} CORR_TYPE={corr_type} E_SUMMARY_METRIC={e_summary_metric} E_THR_P={e_thr_p} SPLIT_MODE={split_mode} VERBOSE=True RANDOMIZE_BEHAVIOR=False; sh {scripts_folder}/S15_cpm_batch.sh".format(scripts_folder = SCRIPTS_DIR,
                            behav_path       = osp.join(RESOURCES_CPM_DIR,'behav_data.csv'),
-                           fc_path          = osp.join(RESOURCES_CPM_DIR,f'fc_data_{ATLAS}.csv'),
+                           fc_path          = osp.join(RESOURCES_CPM_DIR,f'fc_data_{ATLAS_NAME}.csv'),
                            output_dir       = out_dir,
                            behavior         = TARGET,
                            k                = 10,
@@ -136,8 +136,8 @@ for TARGET in targets:
 # ## 2. Swarm jobs for the Null Distributions
 
 for TARGET in targets:    
-    swarm_path[TARGET]  = osp.join(swarm_folder,'S15_CPM-{atlas}-null-{sm}-{conf}-{mt}-{target}.SWARM.sh'.format(atlas=ATLAS,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
-    logdir_path[TARGET] = osp.join(logs_folder, 'S15_CPM-{atlas}-null-{sm}-{conf}-{mt}-{target}.logs'.format(atlas=ATLAS,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
+    swarm_path[TARGET]  = osp.join(swarm_folder,'S15_CPM-{atlas}-null-{sm}-{conf}-{mt}-{target}.SWARM.sh'.format(atlas=ATLAS_NAME,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
+    logdir_path[TARGET] = osp.join(logs_folder, 'S15_CPM-{atlas}-null-{sm}-{conf}-{mt}-{target}.logs'.format(atlas=ATLAS_NAME,sm=SPLIT_MODE,conf=CONFOUNDS,mt=MODEL_TYPE, target=TARGET))
 
 # create specific folders if needed
 # ======================================
@@ -153,16 +153,16 @@ for TARGET in targets:
     swarm_file.write('#Create Time: %s' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     swarm_file.write('\n')
     # Insert comment line with SWARM command
-    swarm_file.write('#swarm -f {swarm_path} -g 8 -t 8 -b 50 --time 00:04:30 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path[TARGET],logdir_path=logdir_path[TARGET]))
+    swarm_file.write('#swarm -f {swarm_path} -g 8 -t 8 -b 15 --time 00:16:00 --partition quick,norm --logdir {logdir_path}'.format(swarm_path=swarm_path[TARGET],logdir_path=logdir_path[TARGET]))
     swarm_file.write('\n')
     for n_iter in range(CPM_NULL_NITERATIONS):
-        out_dir = osp.join(RESOURCES_CPM_DIR,'swarm_outputs','null',ATLAS,SPLIT_MODE,CONFOUNDS,MODEL_TYPE,TARGET)
+        out_dir = osp.join(RESOURCES_CPM_DIR,'swarm_outputs','null',ATLAS_NAME,SPLIT_MODE,CONFOUNDS,MODEL_TYPE,TARGET)
         #out_dir = osp.join(RESOURCES_CPM_DIR,SPLIT_MODE,'null_distribution',behavior,CORR_TYPE,E_SUMMARY_METRIC)
         if not osp.exists(out_dir):
             os.makedirs(out_dir)
         swarm_file.write("export BEHAV_PATH={behav_path} FC_PATH={fc_path} OUT_DIR={output_dir} BEHAVIOR={behavior} NUM_FOLDS={k} NUM_ITER={n_iter} CORR_TYPE={corr_type} E_SUMMARY_METRIC={e_summary_metric} E_THR_P={e_thr_p} SPLIT_MODE={split_mode} VERBOSE=True RANDOMIZE_BEHAVIOR=True; sh {scripts_folder}/S15_cpm_batch.sh".format(scripts_folder = SCRIPTS_DIR,
                            behav_path       = osp.join(RESOURCES_CPM_DIR,'behav_data.csv'),
-                           fc_path          = osp.join(RESOURCES_CPM_DIR,f'fc_data_{ATLAS}.csv'),
+                           fc_path          = osp.join(RESOURCES_CPM_DIR,f'fc_data_{ATLAS_NAME}.csv'),
                            output_dir       = out_dir,
                            behavior         = TARGET,
                            k                = 10,
