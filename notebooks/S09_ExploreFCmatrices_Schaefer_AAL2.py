cfae370cecc39f63eb12e77fdeef9131e27dca2b
javiergcas <javiergcas@gmail.com>
2023-03-10 14:35:50 -0500

    now we can also explore subject averaged matrices

--- notebooks/S09_ExploreFCmatrices_Schaefer_AAL2.py
+++ notebooks/S09_ExploreFCmatrices_Schaefer_AAL2.py
@@ -35,7 +35,7 @@ import matplotlib.pyplot as plt
 #import holoviews as hv
 from utils.basics import get_sbj_scan_list
 from scipy.spatial.distance import squareform
-from utils.basics import DATA_DIR, CORTICAL_400ROI_ATLAS_NAME, FB_400ROI_ATLAS_NAME, ATLASES_DIR
+from utils.basics import DATA_DIR, CORTICAL_400ROI_ATLAS_NAME, FB_400ROI_ATLAS_NAME, ATLASES_DIR, FB_200ROI_ATLAS_NAME
 from utils.plotting import hvplot_fc, plot_fc
 from sfim_lib.io.afni import load_netcc
 from scipy.spatial.distance import cosine      as cosine_distance
@@ -49,13 +49,15 @@ import os
 port_tunnel = int(os.environ['PORT2'])
 print('++ INFO: Second Port available: %d' % port_tunnel)
 
+ATLAS_NAME = FB_200ROI_ATLAS_NAME
+
 # # 1. Load the final list of scans used in this project
 
 sbj_list, scan_list = get_sbj_scan_list(when='post_motion', return_snycq=False)
 
 # # 2. Load information about the Atlas and ROI needed for plotting
 
-ATLASINFO_PATH = osp.join(ATLASES_DIR,FB_400ROI_ATLAS_NAME,'{ATLAS_NAME}.roi_info.csv'.format(ATLAS_NAME=FB_400ROI_ATLAS_NAME))
+ATLASINFO_PATH = osp.join(ATLASES_DIR,ATLAS_NAME,f'{ATLAS_NAME}.roi_info.csv')
 roi_info       = pd.read_csv(ATLASINFO_PATH)
 roi_info
 
@@ -63,32 +65,61 @@ roi_info
 #
 # We will place the matrices in two separate xr.DataArray data structures. One will hold the matrices in terms of Pearson's correlation (```all_sfc_R```) and the other one in terms of their Fisher's transform (```all_sfc_Z```). In the first cell below, we create empty versions of these two data structures. These empty data structures will get populated in the subsequent cell.
 
+unique_sbj_ids = list(pd.Series([sbj for sbj,_ in scan_list]).unique())
+unique_run_ids = list(pd.Series([run for _,run in scan_list]).unique())
+
 # Create empty Xr Data Array to hold all FC matrices
-all_sfc_R = xr.DataArray(dims=['Scan','ROI1','ROI2'], 
-                         coords={'Scan':[sbj+'|'+run for sbj,run in scan_list],
-                               'ROI1':roi_info['ROI_Name'].values,
-                               'ROI2':roi_info['ROI_Name'].values})
-all_sfc_Z = xr.DataArray(dims=['Scan','ROI1','ROI2'], 
-                         coords={'Scan':[sbj+'|'+run for sbj,run in scan_list],
-                               'ROI1':roi_info['ROI_Name'].values,
-                               'ROI2':roi_info['ROI_Name'].values})
+all_sfc_R = xr.DataArray(dims=['Subject','Run','ROI1','ROI2'], 
+                         coords={'Subject':unique_sbj_ids,
+                                 'Run': unique_run_ids,
+                                 'ROI1':roi_info['ROI_Name'].values,
+                                 'ROI2':roi_info['ROI_Name'].values})
+all_sfc_Z = xr.DataArray(dims=['Subject','Run','ROI1','ROI2'], 
+                         coords={'Subject':unique_sbj_ids,
+                                 'Run': unique_run_ids,
+                                 'ROI1':roi_info['ROI_Name'].values,
+                                 'ROI2':roi_info['ROI_Name'].values})
 
 # %%time
 all_rois = list(roi_info['ROI_Name'].values)
 # Load all matrices
 for sbj,run in tqdm(scan_list):
     _,_,_,_,run_num,_,run_acq = run.split('-')
-    netcc_path = osp.join(DATA_DIR,'PrcsData',sbj,'preprocessed','func','pb06_staticFC','{run_acq}_run-{run_num}.{ATLAS_NAME}_000.netcc'.format(run_acq = run_acq, run_num = run_num, ATLAS_NAME = FB_400ROI_ATLAS_NAME))
+    netcc_path = osp.join(DATA_DIR,'PrcsData',sbj,'preprocessed','func','pb06_staticFC',f'{run_acq}_run-{run_num}.{ATLAS_NAME}_000.netcc')
     netcc      = load_netcc(netcc_path)
     this_scan_rois = [ item.strip().strip('7Networks_') for item in list(netcc.columns)]
-    all_sfc_R.loc[sbj+'|'+run,:,:] = netcc
-    all_sfc_Z.loc[sbj+'|'+run,:,:] = netcc.apply(np.arctanh)
+    all_sfc_R.loc[sbj,run,:,:] = netcc
+    all_sfc_Z.loc[sbj,run,:,:] = netcc.apply(np.arctanh)
+
+# + active=""
+# # Create empty Xr Data Array to hold all FC matrices
+# all_sfc_R = xr.DataArray(dims=['Scan','ROI1','ROI2'], 
+#                          coords={'Scan':[sbj+'|'+run for sbj,run in scan_list],
+#                                'ROI1':roi_info['ROI_Name'].values,
+#                                'ROI2':roi_info['ROI_Name'].values})
+# all_sfc_Z = xr.DataArray(dims=['Scan','ROI1','ROI2'], 
+#                          coords={'Scan':[sbj+'|'+run for sbj,run in scan_list],
+#                                'ROI1':roi_info['ROI_Name'].values,
+#                                'ROI2':roi_info['ROI_Name'].values})
+
+# + active=""
+# %%time
+# all_rois = list(roi_info['ROI_Name'].values)
+# # Load all matrices
+# for sbj,run in tqdm(scan_list):
+#     _,_,_,_,run_num,_,run_acq = run.split('-')
+#     netcc_path = osp.join(DATA_DIR,'PrcsData',sbj,'preprocessed','func','pb06_staticFC','{run_acq}_run-{run_num}.{ATLAS_NAME}_000.netcc'.format(run_acq = run_acq, run_num = run_num, ATLAS_NAME = FB_400ROI_ATLAS_NAME))
+#     netcc      = load_netcc(netcc_path)
+#     this_scan_rois = [ item.strip().strip('7Networks_') for item in list(netcc.columns)]
+#     all_sfc_R.loc[sbj+'|'+run,:,:] = netcc
+#     all_sfc_Z.loc[sbj+'|'+run,:,:] = netcc.apply(np.arctanh)
+# -
 
 # # 4. Compute the average matrix for the whole sample
 #
 # To compute the sample mean, we first Fisher's transform each individual matrix, average those, and do the inverse transform of the average
 
-REFERENCE_fc = np.tanh(all_sfc_Z.mean(dim='Scan')).values
+REFERENCE_fc = np.tanh(all_sfc_Z.mean(dim=['Subject','Run'])).values
 
 # Put the matrix into a properly annotated DataFrame structure
 
@@ -99,6 +130,21 @@ REFERENCE_fc.columns.name = 'ROI2'
 # Plot the sample mean (or Reference) FC matrix
 
 hvplot_fc(REFERENCE_fc, ATLASINFO_PATH, cbar_title='Average FC for the whole sample')
+
+# # 5. Comput average FC matrix per subject (in case we want to explore those)
+
+sbj_level_sfc_R = np.arctan(all_sfc_Z.mean(dim='Run'))
+
+sbj_select = pn.widgets.Select(name='Subject', options=unique_sbj_ids)
+@pn.depends(sbj_select)
+def plot_subject_fc(sbj):
+    this_subject_mat = pd.DataFrame(sbj_level_sfc_R.loc[sbj].values, index=list(sbj_level_sfc_R.coords['ROI1'].values), columns=list(sbj_level_sfc_R.coords['ROI2'].values))
+    return hvplot_fc(this_subject_mat, ATLASINFO_PATH, cbar_title='FC '+sbj)
+sbj_mat_dashboard = pn.Row(sbj_select, plot_subject_fc)
+
+sbj_mat_dashboard_server = sbj_mat_dashboard.show(port=port_tunnel,open=False)
+
+sbj_mat_dashboard_server.stop()
 
 # ## 4.1. Vectorize Reference FC Matrix
 #
@@ -185,7 +231,7 @@ anat_sorting_blocks
 all_sfc_R_VECT = pd.DataFrame(index=np.arange(Nconns),columns=[sbj+'|'+run for sbj,run in scan_list])
 for sbj,run in tqdm(scan_list):
     scan_label          = sbj+'|'+run 
-    this_scan_fc_matrix = all_sfc_R.loc[scan_label].values
+    this_scan_fc_matrix = all_sfc_R.loc[sbj,run].values
     np.fill_diagonal(this_scan_fc_matrix,0)
     this_scan_fc_vector = squareform(this_scan_fc_matrix)
     all_sfc_R_VECT[scan_label] = this_scan_fc_vector
@@ -308,7 +354,7 @@ def plot_one_scan(scan_num,sorting_method):
     sbj, run                  = scan_name.split('|')
     _,_,_,_,run_num,_,run_acq = (run).split('-')
     scan_label = sbj + ',' + run_num+'_'+run_acq + ' | R = %.3f' % corr_to_REFERENCE[scan_name]  + ' | Cov = %.2f' % cov_to_REFERENCE[scan_name] + ' | DCos = %.2f' % Dcos_to_REFERENCE[scan_name] + ' | DCor = %.2f' % Dcor_to_REFERENCE[scan_name] + ' | DEuc = %.2f' % Deuc_to_REFERENCE[scan_name]
-    data = all_sfc_R.loc[scan_name].values
+    data = all_sfc_R.loc[sbj,run].values
     fig  = plot_fc(data,ATLASINFO_PATH, figsize=(10,10))
     #return pn.Card(fig,title=scan_label,collapsible=False)
     return pn.Card(pn.pane.Matplotlib(fig),title=scan_label,collapsible=False)
@@ -328,7 +374,8 @@ dashboard_server.stop()
 most_different_scan      = corr_to_REFERENCE.sort_values().index[0]
 most_different_scan_corr = corr_to_REFERENCE.sort_values()[0]
 print('INFO: Most dissimilar scan to the sample mean [%s --> R(scan,mean) = %.2f ]' % (most_different_scan,most_different_scan_corr))
-most_different_matrix = all_sfc_R.loc[most_different_scan,:,:].values
+sbj,run = most_different_scan.split('|')
+most_different_matrix = all_sfc_R.loc[sbj,run,:,:].values
 np.fill_diagonal(most_different_matrix,1)
 hvplot_fc(most_different_matrix, ATLASINFO_PATH)
 
@@ -337,7 +384,8 @@ hvplot_fc(most_different_matrix, ATLASINFO_PATH)
 most_similar_scan      = corr_to_REFERENCE.sort_values(ascending=False).index[0]
 most_similar_scan_corr = corr_to_REFERENCE.sort_values(ascending=False)[0]
 print('INFO: Most dissimilar scan to the sample mean [%s --> R(scan,mean) = %.2f ]' % (most_similar_scan,most_similar_scan_corr))
-most_similar_matrix = all_sfc_R.loc[most_similar_scan,:,:].values
+sbj,run = most_similar_scan.split('|')
+most_similar_matrix = all_sfc_R.loc[sbj,run,:,:].values
 np.fill_diagonal(most_similar_matrix,1)
 hvplot_fc(most_similar_matrix, ATLASINFO_PATH)
 
@@ -345,6 +393,7 @@ median_selector          = corr_to_REFERENCE[corr_to_REFERENCE==corr_to_REFERENC
 median_similar_scan      = median_selector.index[0]
 median_similar_scan_corr = median_selector.values
 print('INFO: Scan with median similarity to the sample mean [%s --> R(scan,mean) = %.2f ]' % (median_similar_scan,median_similar_scan_corr))
-median_similar_matrix = all_sfc_R.loc[median_similar_scan,:,:].values
+sbj,run = median_similar_scan.split('|')
+median_similar_matrix = all_sfc_R.loc[sbj,run,:,:].values
 np.fill_diagonal(median_similar_matrix,1)
 hvplot_fc(median_similar_matrix, ATLASINFO_PATH)
