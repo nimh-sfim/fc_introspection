1b591749f90980fdf31b6af0f6fefcf82ed52c63
javiergcas <javiergcas@gmail.com>
2023-04-24 14:54:00 -0400

    Small visual modification to images for initial manuscript submission

--- notebooks/S10_SNYCQ_Exploration.py
+++ notebooks/S10_SNYCQ_Exploration.py
@@ -38,6 +38,7 @@ SBJs, SCANs, SNYCQ = get_sbj_scan_list(when='post_motion', return_snycq=True)
 
 # We now remove the answer to the question about vigilance, as we do not use that question when looking for groups of scans with different experiences.
 
+VIGILANCE = SNYCQ['Vigilance']
 SNYCQ = SNYCQ.drop('Vigilance',axis=1)
 Nscans, Nquestions = SNYCQ.shape
 print(SNYCQ.shape)
@@ -70,8 +71,19 @@ data_to_plot.columns.name = 'Scans'
 x_ticks       = [0, 99, 199, 299, 399, Nscans-1]
 x_tick_labels = ['1','100','200','300','400', str(Nscans)]
 x_ticks_info  = list(tuple(zip(x_ticks,x_tick_labels)))
-f = data_to_plot.hvplot.heatmap(width=400, height=800, cmap='viridis', yticks= x_ticks_info, fontscale=1.5, xlabel='Questions', ylabel='Scans', title='M').opts(xrotation=90, colorbar_opts={'title':'Response Value:'})
-pn.Row(f).save('./figures/S10_M_matrix.png')
+f_data = data_to_plot.hvplot.heatmap(width=375, height=900, cmap='viridis', yaxis=None, fontscale=1.5, xlabel='Questions', title='', shared_axes=False).opts(xrotation=90, colorbar_opts={'title':'Response:'}, toolbar=None)
+
+wakefulness_to_plot              = pd.DataFrame(VIGILANCE.values)
+wakefulness_to_plot.index        = np.arange(VIGILANCE.shape[0])
+wakefulness_to_plot.index.name   = 'Questions'
+wakefulness_to_plot.columns      = [' Wakefulness']
+wakefulness_to_plot.columns.name = 'Scans'
+x_ticks       = [0, 99, 199, 299, 399, Nscans-1]
+x_tick_labels = ['1','100','200','300','400', str(Nscans)]
+x_ticks_info  = list(tuple(zip(x_ticks,x_tick_labels)))
+f_wakefulness = wakefulness_to_plot.hvplot.heatmap(width=100, height=900, cmap='viridis', yticks= x_ticks_info, fontscale=1.5, xlabel='', ylabel='Scans', title='').opts(xrotation=90, colorbar=False, toolbar=None)
+
+pn.Row(f_wakefulness,f_data).save('./figures/S10_M_matrix.png')
 
 display.Image('./figures/S10_M_matrix.png')
 
@@ -86,14 +98,33 @@ snycq_corr.columns.name='Questions'
 # We rely on seaborn ```clustermap``` function to sort question according to how correlated they are
 
 sns.set(font_scale=1.2)
-clmap = sns.clustermap(snycq_corr.round(1), annot=True, cmap='RdBu_r', figsize=(8,8), vmin=-.5, vmax=.5, cbar_kws = {'orientation':'horizontal'}, cbar_pos=(0,0,1,.01), method='ward');
+clmap = sns.clustermap(snycq_corr.round(1), annot=True, cmap='RdBu_r', figsize=(8,8), vmin=-.5, vmax=.5, cbar_kws = {'orientation':'horizontal','label':'Correlation'}, cbar_pos=(.15,0,.8,.03), method='ward');
 clmap.ax_row_dendrogram.set_visible(False)
 clmap.ax_col_dendrogram.set_visible(False)
+clmap.ax_heatmap.set_xlabel(None)
+clmap.ax_heatmap.set_ylabel(None)
+
+clmap.savefig('./figures/S10_M_corr.png')
 
 # We automatically extract the order of the questions as grouped by ```clustermap``` above.
 
 sorted_questions = list(SNYCQ.columns[clmap.dendrogram_col.reordered_ind])
 print(sorted_questions)
+
+# We can plot again the questions, but this time sorted by the correlation profile from cluster map
+
+data_to_plot              = pd.DataFrame(SNYCQ[sorted_questions].values)
+data_to_plot.index        = np.arange(SNYCQ.shape[0])
+data_to_plot.index.name   = 'Questions'
+data_to_plot.columns      = SNYCQ.columns
+data_to_plot.columns.name = 'Scans'
+x_ticks       = [0, 99, 199, 299, 399, Nscans-1]
+x_tick_labels = ['1','100','200','300','400', str(Nscans)]
+x_ticks_info  = list(tuple(zip(x_ticks,x_tick_labels)))
+f = data_to_plot.hvplot.heatmap(width=400, height=800, cmap='viridis', yticks= x_ticks_info, fontscale=1.5, xlabel='Questions', ylabel='Scans', title='').opts(xrotation=90, colorbar_opts={'title':'Response:'}, toolbar=None)
+pn.Row(f).save('./figures/S10_M_matrix_sorted.png')
+
+display.Image('./figures/S10_M_matrix_sorted.png')
 
 # ## 1.4. Plot the distribution of answers per question
 
