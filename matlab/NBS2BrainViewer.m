116ea81739faf0c84f9e83a2c0b84acd3a14812b
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2024-05-20 10:42:15 -0400

    Updated NBS results plotting notebook

--- matlab/NBS2BrainViewer.m
+++ matlab/NBS2BrainViewer.m
@@ -2,16 +2,17 @@
 %  ========================================================
 clear all
 RESOURCES_DIR="/data/SFIMJGC_Introspec/2023_fc_introspection/code/fc_introspection/resources/nbs";
-atlases=["Schaefer2018_400Parcels_7Networks_AAL2","Schaefer2018_200Parcels_7Networks_AAL2"];
-scenarios=["All_Scans"];
+atlases=["Schaefer2018_400Parcels_7Networks_AAL2"];%,"Schaefer2018_200Parcels_7Networks_AAL2"];
+%scenarios=["All_Scans"];
+designs=["SbjAware"];
 cluster_solution = "CL02";
 contrasts=["Image-Pos-Others_gt_Surr-Neg-Self","Surr-Neg-Self_gt_Image-Pos-Others"];
-thresholds = ["NBS_3p1","NBS_3p5","FDR_0p05","NBS_3p1_augmented","NBS_3p5_augmented","NBS_2p7_augmented"];
+thresholds = ["NBS_3p1", "FDR_0p05"]; %,"NBS_3p5","FDR_0p05","NBS_3p1_augmented","NBS_3p5_augmented","NBS_2p7_augmented"];
 for atlas = atlases
-    for scenario = scenarios
-        for threshold = thresholds
+    for threshold = thresholds
+        for design = designs
             for contrast = contrasts
-                work_path = fullfile(RESOURCES_DIR,atlas,scenario,"NBS_"+cluster_solution+"_Results",threshold);
+                work_path = fullfile(RESOURCES_DIR,atlas,"NBS_"+cluster_solution+"_Results",threshold,design);
                 mat_file  = fullfile(work_path, "NBS_"+cluster_solution+"_"+contrast+".mat");
                 txt_file  = fullfile(work_path,"NBS_"+cluster_solution+"_"+contrast+".txt");
                 edge_file = fullfile(work_path,"NBS_"+cluster_solution+"_"+contrast+".edge");
@@ -19,19 +20,26 @@ for atlas = atlases
                     % File exists.
                     data = load(mat_file);
                     num_networks = data.nbs.NBS.n;
-                    if num_networks == 1
-                        data = full(cell2mat(data.nbs.NBS.con_mat));
-                    else
-                        data = full(cell2mat(data.nbs.NBS.con_mat(1)));
-                        disp("++ WARNING: Selecting only the first network, but there are more.")
+                    switch num_networks
+                        case 0
+                            disp("++ INFO: ["+atlas+","+design+","+threshold+","+contrast+"] --> No Significant connections.")
+                        case 1
+                            data = full(cell2mat(data.nbs.NBS.con_mat));
+                            data = data + data.';
+                            writematrix(data,txt_file,"Delimiter"," ");
+                            movefile(txt_file,edge_file);
+                            disp("++ INFO: ["+atlas+","+design+","+threshold+","+contrast+"] --> Number of Significant Connections is " + (sum(sum(data))/2) + " originated in " + sum(sum(data)>0) + " nodes.")
+                        otherwise
+                            disp("++ WARNING: Selecting only the first network, but there are more.")
+                            data = full(cell2mat(data.nbs.NBS.con_mat));
+                            data = data + data.';
+                            writematrix(data,txt_file,"Delimiter"," ");
+                            movefile(txt_file,edge_file);
+                            disp("++ INFO: ["+atlas+","+design+","+threshold+","+contrast+"] --> Number of Significant Connections is " + (sum(sum(data))/2) + " originated in " + sum(sum(data)>0) + " nodes.")
                     end
-                    data = data + data.';
-                    writematrix(data,txt_file,"Delimiter"," ");
-                    movefile(txt_file,edge_file);
-                    disp("++ INFO: ["+atlas+","+scenario+","+threshold+","+contrast+"] --> Number of Significant Connections is " + (sum(sum(data))/2) + " originated in " + sum(sum(data)>0) + " nodes.")
                 else
                     % File does not exist.
-                    disp("Data does not exists for ["+atlas+","+scenario+","+threshold+","+contrast+"]");
+                    disp("++ INFO: ["+atlas+","+design+","+threshold+","+contrast+"] --> No file available.")
                 end
             end
         end
--- notebooks/S14_PostNBS_PlotFCdiffs.py
+++ notebooks/S14_PostNBS_PlotFCdiffs.py
@@ -6,22 +6,27 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.4
+#       jupytext_version: 1.15.2
 #   kernelspec:
-#     display_name: FC Introspection (Jan 2023)
+#     display_name: FC Instrospection (2023 | 3.10)
 #     language: python
-#     name: fc_introspection
+#     name: fc_introspection_2023_py310
 # ---
 
 # # Description - Exploration of NBS Results
 #
 # This notebook takes the outputs from running NBS and plots then for interpretation
 
+# +
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import pandas as pd
 import numpy as np
 import os.path as osp
 import hvplot.pandas
-from utils.basics import FB_400ROI_ATLAS_NAME, ATLASES_DIR, RESOURCES_NIMARE_DIR, RESOURCES_CONN_DIR, FB_200ROI_ATLAS_NAME
+from utils.basics import FB_400ROI_ATLAS_NAME as ATLAS_NAME
+from utils.basics import ATLASES_DIR, RESOURCES_NIMARE_DIR, RESOURCES_CONN_DIR, FB_200ROI_ATLAS_NAME, RESOURCES_NBS_DIR
 from utils.plotting import hvplot_fc, hvplot_fc_nwlevel, create_graph_from_matrix, plot_as_graph
 import holoviews as hv
 from holoviews import opts
@@ -33,11 +38,16 @@ from matplotlib.colors import LinearSegmentedColormap
 from sklearn.preprocessing import MinMaxScaler
 from nilearn.image import load_img
 from nilearn import masking
+# -
 
-SOLUTION   = 'CL02'
-SCENARIO   = 'All_Scans'
-THRESHOLD  = 'NBS_3p1'
-ATLAS_NAME = FB_400ROI_ATLAS_NAME
+import os
+port_tunnel = int(os.environ['PORT2'])
+print('++ INFO: Second Port available: %d' % port_tunnel)
+
+SOLUTION      = 'CL02'
+THRESHOLD     = 'NBS_3p1'
+DESIGN_MATRIX = 'SbjAware' #'Basic'
+NBS_CONTRASTS = ['Image-Pos-Others_gt_Surr-Neg-Self','Surr-Neg-Self_gt_Image-Pos-Others']
 
 # # 2. Load information about the Atlas and ROI needed for plotting
 #
@@ -45,6 +55,8 @@ ATLAS_NAME = FB_400ROI_ATLAS_NAME
 
 ATLASINFO_PATH = osp.join(ATLASES_DIR,ATLAS_NAME,'{ATLAS_NAME}.roi_info.csv'.format(ATLAS_NAME=ATLAS_NAME))
 roi_info       = pd.read_csv(ATLASINFO_PATH)
+Nrois          = roi_info.shape[0]
+print(Nrois)
 
 # Count the number of networks and get their names
 
@@ -52,252 +64,45 @@ networks = list(roi_info['Network'].unique())
 print(networks, len(networks))
 
 # Load the connections that are significantly stronger for the contrast: $$Image-Pos-Others > Surr-Neg-Self$$
+# and the contrast: $$Surr-Neg-Self > Image-Pos-Others$$
 
-#data_f1GTf2 = np.loadtxt(f'/data/SFIMJGC_Introspec/2023_fc_introspection/code/fc_introspection/resources/nbs/{ATLAS_NAME}/NBS_{SOLUTION}_Results/NBS_{SOLUTION}_Image-Pos-Others_gt_Surr-Neg-Self.edge')
-data_f1GTf2 = np.loadtxt(f'/data/SFIMJGC_Introspec/2023_fc_introspection/code/fc_introspection/resources/nbs/{ATLAS_NAME}/{SCENARIO}/NBS_{SOLUTION}_Results/{THRESHOLD}/NBS_{SOLUTION}_Image-Pos-Others_gt_Surr-Neg-Self.edge')
-data_f1GTf2 = pd.DataFrame(data_f1GTf2,
-                           index   = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index, 
-                           columns = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index)
+data = {}
+for contrast in NBS_CONTRASTS:
+    aux_path = osp.join(RESOURCES_NBS_DIR,ATLAS_NAME,f'NBS_{SOLUTION}_Results',THRESHOLD,DESIGN_MATRIX,f'NBS_{SOLUTION}_{contrast}.edge')
+    if osp.exists(aux_path):
+        aux_data = np.loadtxt(aux_path)
+        data[contrast]  = pd.DataFrame(aux_data,
+                                         index   = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index, 
+                                         columns = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index)
+        print('++ INFO: Data available for %s' % contrast)
+    else:
+        data[contrast] = pd.DataFrame(np.zeros((Nrois,Nrois)),
+                                     index   = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index, 
+                                     columns = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index)
+        print('++ WARTNING: No results available for %s' % contrast)
 
-# Load the connections that are significantly stronger for the contrast: $$Surr-Neg-Self > Image-Pos-Others$$
-
-data_f2GTf1 = np.loadtxt(f'/data/SFIMJGC_Introspec/2023_fc_introspection/code/fc_introspection/resources/nbs/{ATLAS_NAME}/{SCENARIO}/NBS_{SOLUTION}_Results/{THRESHOLD}/NBS_{SOLUTION}_Surr-Neg-Self_gt_Image-Pos-Others.edge')
-data_f2GTf1 = pd.DataFrame(data_f2GTf1,
-                           index   = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index, 
-                           columns = roi_info.set_index(['Hemisphere','Network','ROI_Name','ROI_ID','RGB']).index)
+data['Both'] = data['Image-Pos-Others_gt_Surr-Neg-Self'] - data['Surr-Neg-Self_gt_Image-Pos-Others']
 
 # We will also write the results of NBS into text format that we can load into CONN to generate the brain views of the results
 
-np.savetxt(osp.join(RESOURCES_CONN_DIR,f'GroupDiffs_f1GTf2_{SCENARIO}_{THRESHOLD}_matrix.txt'),data_f1GTf2.values)
-np.savetxt(osp.join(RESOURCES_CONN_DIR,f'GroupDiffs_f2GTf1_{SCENARIO}_{THRESHOLD}_matrix.txt'),data_f2GTf1.values)
-np.savetxt(osp.join(RESOURCES_CONN_DIR,f'GroupDiffs_Both_{SCENARIO}_{THRESHOLD}_matrix.txt'),data_f1GTf2.values-data_f2GTf1.values)
+for contrast in data.keys():
+    if data[contrast] is not None:
+        aux_path = osp.join(RESOURCES_CONN_DIR,f'NBS_{DESIGN_MATRIX}_{THRESHOLD}_{contrast}.txt')
+        np.savetxt(aux_path,data[contrast].values)
+        print("++ INFO: Contrast data [%s] saved to disk %s" %(contrast,aux_path))
 
-# ## 2.1. Plot results as regular FC matrices
-# Plot the NBS results in matrix form in three different ways: 
-# 1) connections for both contrasts
-# 2) Connections for ```Image-Pos-Others > Surr-Neg-Self``` only
-# 3) Connections for ```Surr-Neg-Self > Image-Pos-Others``` only
+# # Plot results at the individual connection level
 
-data = data_f1GTf2 - data_f2GTf1
+hvplot_fc(data['Both'].loc[:,networks,:].T.loc[:,networks,:].T, by='Network', add_color_segments=True, add_labels=True, cmap=['#4472C4','#ffffff','#ED7D31'], major_label_overrides={-0.5:'Surr-Neg-Self > Images-Pos-Others',0:'',0.5:'Images-Pos-Others > Surr-Neg-Self'}, colorbar_position='top').opts(toolbar=None, title=f'{DESIGN_MATRIX} | {THRESHOLD} | Both')
 
-((data_f2GTf1).sum() > 0).sum()
+plot_as_graph(data['Surr-Neg-Self_gt_Image-Pos-Others'], edge_weight=.5, show_hemi_labels=False,pos_edges_color='k')
 
-((data_f1GTf2).sum() > 0).sum()
+data['Surr-Neg-Self_gt_Image-Pos-Others'].sum(axis=1).sort_values(ascending=False)
 
-f_fullmodel_matrix = hvplot_fc(data.loc[:,networks,:].T.loc[:,networks,:].T, by='Network', add_color_segments=True, add_labels=True, cmap=['#4472C4','#ffffff','#ED7D31'], major_label_overrides={-0.5:'Surr-Neg-Self > Images-Pos-Others',0:'',0.5:'Images-Pos-Others > Surr-Neg-Self'}, colorbar_position='top').opts(toolbar=None)
-f_fullmodel_matrix #pn.Row(f_fullmodel_matrix).save('./figures/S14_NBS_FullModel_MatrixView.png')
+hvplot_fc_nwlevel(data['Surr-Neg-Self_gt_Image-Pos-Others'], title='', add_net_colors=True, add_net_labels='both', mode='count', cmap='Greys', clim_max=100, labels_text_color='Greys_r').opts(toolbar=None)
 
-display.Image('./figures/S14_NBS_FullModel_MatrixView.png')
-
-# We can also explore the two contrast separately in this form of representing the results
-
-f = (hvplot_fc(data_f1GTf2.loc[:,networks,:].T.loc[:,networks,:].T, by='Network', add_color_segments=True, add_labels=True, cmap=['#4472C4','#ffffff','#ED7D31'], major_label_overrides={-0.5:'Surr-Neg-Self > Images-Pos-Others',0:'',0.5:'Images-Pos-Others > Surr-Neg-Self'}) + \
-hvplot_fc(-data_f2GTf1.loc[:,networks,:].T.loc[:,networks,:].T, by='Network', add_color_segments=True, add_labels=True, cmap=['#4472C4','#ffffff','#ED7D31'], major_label_overrides={-0.5:'Surr-Neg-Self > Images-Pos-Others',0:'',0.5:'Images-Pos-Others > Surr-Neg-Self'})).opts(toolbar=None)
-f #pn.Row(f).save('./figures/S14_NBS_TwoSeparateModels_MatrixView.png')
-
-display.Image('./figures/S14_NBS_TwoSeparateModels_MatrixView.png')
-
-# ## 2.2. Plot results as counts of inter- and between- network connections
-#
-# To get a better feeling of what networks have more/less significant connections, we will plot a summary view of the results with counts of significant connections per contrast at the network level (instead of ROI level)
-
-f_F1gtF2 = hvplot_fc_nwlevel(data_f1GTf2, title='', add_net_colors=True, add_net_labels=True, mode='count', cmap='oranges', clim_max=50, labels_text_color='black').opts(toolbar=None)
-f_F2gtF1 = hvplot_fc_nwlevel(data_f2GTf1, title='', add_net_colors=True, add_net_labels=True, mode='count', cmap='blues', clim_max=50, labels_text_color='gray').opts(toolbar=None)
-#pn.Row(f_F1gtF2).save('./figures/S14_NBS_F1gtF2_nw_counts.png')
-#pn.Row(f_F2gtF1).save('./figures/S14_NBS_F2gtF1_nw_counts.png')
-
-hvplot_fc_nwlevel(data_f1GTf2, title='')
-
-f_F1gtF2
-
-f_F2gtF1
-
-display.Image('./figures/S14_NBS_F1gtF2_nw_counts.png')
-
-display.Image('./figures/S14_NBS_F2gtF1_nw_counts.png')
-
-f_both = hvplot_fc_nwlevel(data_f1GTf2.abs()+data_f2GTf1.abs(), title='', add_net_colors=True, add_net_labels=True, mode='count', cmap='greys', clim_max=250, labels_text_color='black').opts(toolbar=None)
-pn.Row(f_both).save('./figures/S14_NBS_BOTH_nw_counts.png')
-
-display.Image('./figures/S14_NBS_BOTH_nw_counts.png')
-
-data = (data_f1GTf2.abs()+data_f2GTf1.abs()).copy()
-networks     = list(data.index.get_level_values('Network').unique())
-num_networks = len(networks)
-num_sig_cons = pd.DataFrame(index=networks, columns=networks)
-pc_sig_cons  = pd.DataFrame(index=networks, columns=networks)
-ncons_df     = pd.DataFrame(index=networks, columns=networks)
-for n1 in networks:
-    for n2 in networks:
-        aux = data.loc[data.index.get_level_values('Network')==n1,data.columns.get_level_values('Network')==n2]
-        if n1 == n2:
-            # Within Network Scenario
-            assert np.diagonal(aux).sum() == 0., "WARNING --> Diagonal is not zero for intra-network FC"
-            ncons                   = aux.shape[0] * (aux.shape[0] -1 ) / 2
-            ncons_df.loc[n1,n2]     = int(ncons)
-            num_sig_cons.loc[n1,n2] = int(aux.sum().sum()/2) 
-            assert aux.sum().sum()/2 == np.floor(aux.sum().sum()/2), "WARNING --> Matrix was not symmetric"
-            pc_sig_cons.loc[n1,n2]  = 100 * num_sig_cons.loc[n1,n2] / ncons
-        else:
-            # Across Network Scenario
-            #assert np.diagonal(aux).sum() == 0., "WARNING --> Diagonal is not zero for intra-network FC"
-            ncons = aux.shape[0] * aux.shape[1]
-            ncons_df.loc[n1,n2] = int(ncons)
-            num_sig_cons.loc[n1,n2] = int(aux.sum().sum())
-            pc_sig_cons.loc[n1,n2]  = 100 * num_sig_cons.loc[n1,n2] / ncons
-num_sig_cons = num_sig_cons.infer_objects()
-pc_sig_cons  = pc_sig_cons.infer_objects()
-
-a = ncons_df.hvplot.heatmap(clim=(0,10000)).opts(toolbar=None)
-a*hv.Labels(a)
-
-a = pd.DataFrame(num_sig_cons.sum(axis=1), columns=['Total']).hvplot.heatmap(width=120, cmap='greys', clim=(0,1000)).opts(colorbar=False, toolbar=None)
-b = a*hv.Labels(a)
-pn.Row(b).save('./figures/S14_NBS_BOTH_nw_total_counts.png')
-
-display.Image('./figures/S14_NBS_BOTH_nw_total_counts.png')
-
-(100*num_sig_cons / ncons_df)
-
-pc_sig_cons.sum(axis=1).sort_values()
+hvplot_fc_nwlevel(data['Image-Pos-Others_gt_Surr-Neg-Self'], title='', add_net_colors=True, add_net_labels='y', mode='count', cmap='Reds', clim_max=30, labels_text_color='Reds_r').opts(toolbar=None)
 
 
 
-a = pd.DataFrame(pc_sig_cons.sum(axis=1), columns=['%']).round(1).hvplot.heatmap(width=120, cmap='greys', clim=(0,30)).opts(colorbar=False, toolbar=None)
-b = a*hv.Labels(a)
-pn.Row(b).save('./figures/S14_NBS_BOTH_nw_total_pc.png')
 
-display.Image('./figures/S14_NBS_BOTH_nw_total_pc.png')
-
-# ## 2.3. Plot results as percentage of inter- and intra-network connections
-#
-# Similar view to the one above, but this time instead of reporting the number of significant connecitons, we report the percentage of those relative to the total number of inter- or intra-connections for a particular cell on the matrix
-
-f_F1gtF2 = hvplot_fc_nwlevel(data_f1GTf2, title='', add_net_colors=True, add_net_labels=True, mode='percent', cmap='oranges', clim_max=5, labels_text_color='black').opts(toolbar=None)
-f_F2gtF1 = hvplot_fc_nwlevel(data_f2GTf1, title='', add_net_colors=True, add_net_labels=True, mode='percent', cmap='blues', clim_max=5, labels_text_color='gray').opts(toolbar=None)
-pn.Row(f_F1gtF2).save('./figures/S14_NBS_F1gtF2_nw_percent.png')
-pn.Row(f_F2gtF1).save('./figures/S14_NBS_F2gtF1_nw_percent.png')
-
-display.Image('./figures/S14_NBS_F1gtF2_nw_percent.png')
-
-display.Image('./figures/S14_NBS_F2gtF1_nw_percent.png')
-
-# ## 2.4. Plot results in glass brains
-#
-# We now plot the results of the NBS analysis as glass brains using nilearn's ```plot_connection``` function
-#
-# ### 2.4.1. All Significant connections (both contrast directions)
-
-G, G_att      = create_graph_from_matrix(data)
-N_rois        = G_att.shape[0]
-G_att['Size'] = MinMaxScaler(feature_range=(0,200)).fit_transform(G_att['Degree'].values.reshape(-1,1))
-
-fig, ax            = plt.subplots(1,1,figsize=(15,7))
-fig_NBS_both_brain = plot_connectome(data,roi_info[['pos_R','pos_A','pos_S']],
-                                   node_color=roi_info['RGB'], 
-                                   node_size=G_att['Size'], 
-                                   edge_kwargs={'linewidth':.5}, 
-                                   edge_cmap=LinearSegmentedColormap.from_list('nbs_model',['#4472C4','#ED7D31'],N=2), 
-                                   node_kwargs={'linewidth':1, 'edgecolor':'k'}, axes=ax)
-fig_NBS_both_brain.savefig('./figures/S14_NBS_GlassBrain_FullModel.png')
-
-f = pn.Row(pn.pane.DataFrame(G_att[['ROI_Name','Degree']].sort_values(by='Degree', ascending=False)[0:10],width=500),
-       G_att['Degree'].hvplot.box(width=300, color='gray').opts(toolbar=None))
-f.save('./figures/S14_NBS_HighDegree_FullModel.png')
-
-display.Image('./figures/S14_NBS_HighDegree_FullModel.png')
-
-# ### 2.4.2. Significant Connections only for the contrast Large F1 > Large F2
-
-G, G_att      = create_graph_from_matrix(data_f1GTf2)
-N_rois        = G_att.shape[0]
-G_att['Size'] = MinMaxScaler(feature_range=(0,200)).fit_transform(G_att['Degree'].values.reshape(-1,1))
-
-fig, ax            = plt.subplots(1,1,figsize=(15,7))
-fig_NBS_f1GTf2_brain = plot_connectome(data_f1GTf2,roi_info[['pos_R','pos_A','pos_S']],
-                                   node_color=roi_info['RGB'], 
-                                   node_size=G_att['Size'], 
-                                   edge_kwargs={'linewidth':.5, 'color':'#ED7D31'}, 
-                                   node_kwargs={'linewidth':1, 'edgecolor':'k'}, axes=ax)
-fig_NBS_f1GTf2_brain.savefig('./figures/S14_NBS_GlassBrain_f1GTf2.png')
-
-f = pn.Row(pn.pane.DataFrame(G_att[['ROI_Name','Degree']].sort_values(by='Degree', ascending=False)[0:10],width=500),
-       G_att['Degree'].hvplot.box(width=300, color='#ED7D31').opts(toolbar=None))
-f.save('./figures/S14_NBS_HighDegree_f1GTf2.png')
-
-display.Image('./figures/S14_NBS_HighDegree_f1GTf2.png')
-
-# ### 2.4.2. Significant Connections only for the contrast Large F2 > Large F1
-
-G, G_att      = create_graph_from_matrix(data_f2GTf1)
-N_rois        = G_att.shape[0]
-G_att['Size'] = MinMaxScaler(feature_range=(0,200)).fit_transform(G_att['Degree'].values.reshape(-1,1))
-
-fig, ax            = plt.subplots(1,1,figsize=(15,7))
-fig_NBS_f2GTf1_brain = plot_connectome(data_f2GTf1,roi_info[['pos_R','pos_A','pos_S']],
-                                   node_color=roi_info['RGB'], 
-                                   node_size=G_att['Size'], 
-                                   edge_kwargs={'linewidth':.5, 'color':'#4472C4'}, 
-                                   node_kwargs={'linewidth':1, 'edgecolor':'k'}, axes=ax)
-fig_NBS_f2GTf1_brain.savefig('./figures/S14_NBS_GlassBrain_f2GTf1.png')
-
-f = pn.Row(pn.pane.DataFrame(G_att[['ROI_Name','Degree']].sort_values(by='Degree', ascending=False)[0:10],width=500),
-       G_att['Degree'].hvplot.box(width=300, color='#4472C4').opts(toolbar=None))
-f.save('./figures/S14_NBS_HighDegree_f2GTf1.png')
-
-display.Image('./figures/S14_NBS_HighDegree_f2GTf1.png')
-
-# ## 2.5. Plot NBS results as Circos Plots
-#
-# ### 2.5.1. Full Model
-
-f = plot_as_graph(data,layout='circos',edge_weight=.2, show_degree=True)
-f
-
-f.savefig('./figures/S14_NBS_Circos_FullModel.png')
-
-# ### 2.5.2. Image-Pos-Others > Surr-Neg-Self
-
-f = plot_as_graph(data_f1GTf2,layout='circos',edge_weight=.2, show_degree=True)
-f
-
-f.savefig('./figures/S14_NBS_Circos_f1GTf2.png')
-
-# ### 2.5.2. Surr-Neg-Self > Image-Pos-Others
-
-f = plot_as_graph(-data_f2GTf1,layout='circos',edge_weight=.2, show_degree=True)
-f
-
-f.savefig('./figures/S14_NBS_Circos_f2GTf1.png')
-
-# # 10. Laterality Index for each contrast
-#
-# First, we compute the fcLI for the ```Images-Pos-Others > Surr-Neg-Self``` contrast
-
-aux = (data_f1GTf2).copy()
-aux.index = data_f1GTf2.index.get_level_values('Hemisphere')
-aux.columns = data_f1GTf2.columns.get_level_values('Hemisphere')
-f1GTf2_LL = (aux.loc['LH','LH'].sum().sum() / 2)
-f1GTf2_RR = (aux.loc['RH','RH'].sum().sum() / 2)
-f1GTf2_LR = aux.loc['LH','RH'].sum().sum()
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] L-L Conns: %d' % f1GTf2_LL)
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] R-R Conns: %d' % f1GTf2_RR)
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] R-L Conns: %d' % f1GTf2_LR)
-print('++ --------------------------------------------------------')
-f1GTf2_fcLI  = (f1GTf2_LL - f1GTf2_RR) / (f1GTf2_LL + f1GTf2_RR)
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] fcLI:      %.2f' % f1GTf2_fcLI)
-
-# Next, we do the same for the ```Surr-Neg-Self > Images-Pos-Others``` contrast
-
-aux = (data_f2GTf1).copy()
-aux.index = data_f2GTf1.index.get_level_values('Hemisphere')
-aux.columns = data_f2GTf1.columns.get_level_values('Hemisphere')
-f2GTf1_LL = (aux.loc['LH','LH'].sum().sum() / 2)
-f2GTf1_RR = (aux.loc['RH','RH'].sum().sum() / 2)
-f2GTf1_LR = aux.loc['LH','RH'].sum().sum()
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] L-L Conns: %d' % f2GTf1_LL)
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] R-R Conns: %d' % f2GTf1_RR)
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] R-L Conns: %d' % f2GTf1_LR)
-print('++ --------------------------------------------------------')
-f2GTf1_fcLI  = (f2GTf1_LL - f2GTf1_RR) / (f2GTf1_LL + f2GTf1_RR)
-print('++ INFO [Image-Pos-Others > Surr-Neg-Self] fcLI:      %.2f' % f2GTf1_fcLI)
