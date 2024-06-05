cc5374114e27704fb9d66fb28abfb5f483e29ab1
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2024-06-05 08:33:59 -0400

    Updated NBS Lit review scripts

--- notebooks/S15_NBS_Literature_Search_Results.py
+++ notebooks/S15_NBS_Literature_Search_Results.py
@@ -13,6 +13,10 @@
 #     name: fc_introspection_2023_py310
 # ---
 
+# # Description
+#
+# This notebook creates the figure that use to show how our results relate to prior work that has used NBS to look for significant differences in connectivity between clinical and healthy populations.
+
 import pandas as pd
 import numpy as np
 import hvplot.pandas
@@ -35,23 +39,6 @@ nbs_lit['% Sig. Nodes'] = nbs_lit['% Sig. Nodes'].str.rstrip('%').astype('float'
 
 fig, axs = plt.subplots(1,2,figsize=(8,5))
 # Percent Significant Connections in NBS analysis
-sns.boxplot(data=nbs_lit,y='% Sig. Edges', color='lightblue', ax=axs[0])
-sns.swarmplot(data=nbs_lit,y='% Sig. Edges',c ='b', s=2,ax=axs[0])
-axs[0].set_ylabel('Percentage of Significant Edges')
-axs[0].axhline(y=100*590/(380*379/2), color='r', linestyle='--', linewidth=1)
-axs[0].text(-0.05, 1, 'a', transform=axs[0].transAxes,
-      fontsize=14, fontweight='bold', va='top', ha='right')
-# Percentage of Significant Nodes in NBS analysis
-sns.boxplot(data=nbs_lit,y='% Sig. Nodes', color='lightblue', ax=axs[1])
-sns.swarmplot(data=nbs_lit,y='% Sig. Nodes',c ='b', s=2, ax=axs[1])
-axs[1].set_ylabel('Percentage of Significant Nodes')
-axs[1].axhline(y=100*221/380, color='r', linestyle='--', linewidth=1)
-fig.suptitle('Comparison to clinical studies using NBS', fontsize=14, fontweight='bold')
-axs[1].text(-0.05, 1, 'b', transform=axs[1].transAxes,
-      fontsize=14, fontweight='bold', va='top', ha='right')
-
-fig, axs = plt.subplots(1,2,figsize=(8,5))
-# Percent Significant Connections in NBS analysis
 sns.boxplot(data=nbs_lit,y='% Sig. Edges', color='lightgray', ax=axs[0])
 sns.swarmplot(data=nbs_lit,y='% Sig. Edges',c ='k', s=2,ax=axs[0])
 axs[0].set_ylabel('Percentage of Significant Edges')
@@ -61,6 +48,3 @@ sns.boxplot(data=nbs_lit,y='% Sig. Nodes', color='lightgray', ax=axs[1])
 sns.swarmplot(data=nbs_lit,y='% Sig. Nodes',c ='k', s=2, ax=axs[1])
 axs[1].set_ylabel('Percentage of Nodes in Significant Edges')
 axs[1].axhline(y=100*221/380, color='r', linestyle='--', linewidth=1)
-
-
-
