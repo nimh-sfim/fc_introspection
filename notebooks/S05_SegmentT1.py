779cb7ab53bbf25669b9218f0bc8ea9739bb6c8f
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2026-05-07 11:48:58 -0400

    New markup

--- notebooks/S05_SegmentT1.py
+++ notebooks/S05_SegmentT1.py
@@ -1,23 +1,13 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.15.2
-#   kernelspec:
-#     display_name: FC Instrospection py 3.10 | 2023b
-#     language: python
-#     name: fc_introspection_2023b_py310
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
-# # Description - Create Swarm File to run transformation to MNI pipeline on the preprocessed data
-#
-# This script creates the SWARM file to run the pipeline that will transform the data to MNI space, which is necessary for the following step of this project.
+# # Description - Create Swarm File to extrtact GM, WM and CSF tissue masks
+# 
+# This script will call AFNI program `3dSeg` on each subject's T1 already in MNI space to extract masks for GM, WM and CSF.
 
-# +
+# In[7]:
+
+
 import pandas as pd
 import os.path as osp
 import os
@@ -31,23 +21,33 @@ from utils.basics import PRJ_DIR, DATA_DIR, SCRIPTS_DIR
 print('++ INFO: Project Dir:                  %s' % PRJ_DIR) 
 print('++ INFO: Bash Scripts Dir:             %s' % SCRIPTS_DIR)
 print('++ INFO: Data Dir:                     %s' % DATA_DIR)
-# -
+
+
+# In[8]:
+
 
 username = getpass.getuser()
 print('++ INFO: user working now --> %s' % username)
 
+
 # # 1. Load list of scans that completed struct and func pre-processing and have low motion
+
+# In[9]:
+
 
 sbj_list, scan_list, SNYCQ_data = get_sbj_scan_list('post_motion')
 
+
 # ***
 # # 2. Create SWARM file
-#
+# 
 # This will create a swarm file with one line call per subject. The inputs to that bash script are:
-#
+# 
 # * SBJ = subject ID
 
-# +
+# In[10]:
+
+
 #user specific folders
 #=====================
 swarm_folder   = osp.join(PRJ_DIR,'SwarmFiles.{username}'.format(username=username))
@@ -55,7 +55,10 @@ logs_folder    = osp.join(PRJ_DIR,'Logs.{username}'.format(username=username))
 
 swarm_path     = osp.join(swarm_folder,'S05_SegmentT1.SWARM.sh')
 logdir_path    = osp.join(logs_folder, 'S05_SegmentT1.logs')
-# -
+
+
+# In[11]:
+
 
 # create user specific folders if needed
 # ======================================
@@ -66,7 +69,10 @@ if not osp.exists(logdir_path):
     os.makedirs(logdir_path)
     print('++ INFO: New folder for log files created [%s]' % logdir_path)
 
-# +
+
+# In[14]:
+
+
 # Open the file
 swarm_file = open(swarm_path, "w")
 # Log the date and time when the SWARM file is created
@@ -81,3 +87,4 @@ for sbj in sbj_list:
     swarm_file.write("export SBJ={sbj}; sh {scripts_folder}/S05_SegmentT1.sh".format(sbj=sbj, scripts_folder = SCRIPTS_DIR))
     swarm_file.write('\n')
 swarm_file.close()
+
